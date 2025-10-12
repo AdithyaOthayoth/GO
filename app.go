cf1aa2b17402c1a5dd1775a9c3beceaa896f5163
Adithya <adithyaudayo05@gmail.com>
2025-10-12 22:27:32 +0100

    Investment calculator

--- app.go
+++ app.go
@@ -1,5 +1,23 @@
-package main
-import "fmt"
-func main(){
-	fmt.Print("Hello World")
+package main //main tells go that main package is the main entry point
+import (
+	"fmt"
+	"math"
+)
+
+// uint - non negative integer data type, int=0, string="", float64=0.0, bool= false, int32
+func main() {
+	const inflationRate = 2.5
+	var years float64 = 10
+	var investmentAmount float64
+	expectedReturnRate := 5.5 //short notation to declare a variable
+
+	//input
+	fmt.Print("Please enter the Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	//output
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
